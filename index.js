450dec501db9282d7d1aa11d73aa3be18ae4cf32
Sonam Pal <spal@ukcloud.com>
2022-10-20 11:17:16 +0100

    changes to json

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const port  = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).json('Hello world from Travis')
+  res.status(200).json('Hello world from Travis! CICD challenge')
 })
 
 // Exporting the home route so it can be tested
